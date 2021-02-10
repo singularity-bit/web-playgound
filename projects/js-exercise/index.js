af4985dbd5daacb3da83083f3cf6fba4b81cedff
vasile <58362597+singularity-bit@users.noreply.github.com>
2021-02-10 17:30:29 +0200

    Delete projects directory

--- projects/js-exercise/index.js
+++
@@ -1,8 +0,0 @@
-var input =document.getElementById("item");
-var button=document.getElementById("enter");
-var ul=document.querySelector("ul");
-button.addEventListener("click",function(){
-    var li=document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-})
